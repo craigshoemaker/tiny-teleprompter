ffb1c1dc4f9c9240d0616bca5cba953779decb58
craigshoemaker <craig@craigshoemaker.net>
2020-07-28 10:26:20 -0400

    Streamline script

--- tiny-teleprompter.js
+++ tiny-teleprompter.js
@@ -1,14 +1,10 @@
 (() => {
   let currentElement = document.querySelector("p");
-  const goTo = (el) => el.scrollIntoView(true);
   const advance = (direction) => {
     currentElement = currentElement[direction + "ElementSibling"];
-    if (!currentElement) {
-      currentElement = document.querySelector("p");
-    }
-    goTo(currentElement);
+    if (!currentElement) currentElement = document.querySelector("p");
+    currentElement.scrollIntoView(true);
   };
-
   document.body.onkeyup = (e) => {
     const keyCode = e.keyCode.toString();
     if (/32|39|77/.test(keyCode)) advance("next");
